9c54774b2a4df78bc32bcc43d3f34631a51d00e5
Apoorv <apoorvsingal0@gmail.com>
2022-01-09 15:22:04 +0530

    export Issue

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,20 @@ import GithubClient from "./lib/GithubClient";
 import BackendClient from "./lib/BackendClient";
 import MessageClient from "./lib/MessageClient";
 import WebhookClient from "./lib/WebhookClient";
+import Issue from "./lib/Issue";
 
 export * from "./lib/MessageClient";
 export * from "./lib/HttpClient";
 export * from "./lib/BackendClient";
 export * from "./lib/GithubClient";
 export * from "./lib/WebhookClient";
+export * from "./lib/Issue";
 
 export {
   HttpClient,
   GithubClient,
   BackendClient,
   MessageClient,
-  WebhookClient
+  WebhookClient,
+  Issue
 };
