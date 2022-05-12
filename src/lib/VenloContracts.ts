efc3ca94593bf10c74b4e8786cc6af4e2bc79c3f
Apoorv <apoorvsingal0@gmail.com>
2022-05-12 14:37:43 +0530

    oops

---
+++ src/lib/VenloContracts.ts
@@ -0,0 +1,44 @@
+
+import CreatorToken from "../contracts/CreatorToken.json";
+import CreatorTokenFactory from "../contracts/CreatorTokenFactory.json";
+import Repository from "../contracts/Repository.json";
+import RepoFactory from "../contracts/RepoFactory.json";
+import Ven from "../contracts/Ven.json";
+import UserTable from "../contracts/UserTable.json";
+import RepoTable from "../contracts/RepoTable.json";
+
+export enum ChainId {
+  Polygon = 137,
+  Mumbai = 80001,
+  Localhost = 1337,
+};
+
+const contracts = {
+  [ChainId.Mumbai]: {
+    CreatorToken: { abi: CreatorToken.abi },
+    Repository: { abi: Repository.abi },
+
+    CreatorTokenFactory: CreatorTokenFactory.networks[ChainId.Mumbai] && {
+      abi: CreatorTokenFactory.abi,
+      address: CreatorTokenFactory.networks[ChainId.Mumbai].address
+    },
+    RepoFactory: RepoFactory.networks[ChainId.Mumbai] && {
+      abi: RepoFactory.abi,
+      address: RepoFactory.networks[ChainId.Mumbai].address
+    },
+    Ven: Ven.networks[ChainId.Mumbai] && {
+      abi: Ven.abi,
+      address: Ven.networks[ChainId.Mumbai].address
+    },
+    UserTable: UserTable.networks[ChainId.Mumbai] && {
+      abi: UserTable.abi,
+      address: UserTable.networks[ChainId.Mumbai].address
+    },
+    RepoTable: RepoTable.networks[ChainId.Mumbai] && {
+      abi: RepoTable.abi,
+      address: RepoTable.networks[ChainId.Mumbai].address
+    },
+  }
+};
+
+export default contracts;
